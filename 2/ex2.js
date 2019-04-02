3eae22ecf109d237a4ba6c148bb3edcc7ce11643
Halison da Silva Brito <halisonbrito@gmail.com>
2019-04-02 01:31:00 -0400

    1 Assignment

--- 2/ex2.js
+++
@@ -1,9 +0,0 @@
-function isWeekend(){
-  const todayDate = new Date();
-  const day = todayDate.getDay();
-
-  let daysText = ['weekend','weekday','weekday','weekday','weekday','weekday','weekend'];
-  
-  return daysText[day]; 		
-	
-}
