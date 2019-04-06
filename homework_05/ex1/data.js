e32eb39df485ae016555ae5402d6c662a40375ba
Halison da Silva Brito <halisonbrito@gmail.com>
2019-04-05 23:22:15 -0400

    Homework_05 finished

--- homework_05/ex1/data.js
+++ homework_05/ex1/data.js
@@ -4,6 +4,8 @@ async function loadFile(callback){
 
     var dataQuery = await axios.get('https://randomuser.me/api/?results=10');
 
+    console.log(dataQuery);
+
     callback(dataQuery);
    
 }
--- homework_05/ex1/server.js
+++ homework_05/ex1/server.js
@@ -1,27 +1,33 @@
 const express = require('express');
-const dataQuery = require('./data')
+const dataQuery = require('./data');
+const moment = require('moment');
 
 
 const app = express();
-//https://developer.github.com/v3/#paginationy
+
+app.enable('case sensitive routing');
+app.enable('strict routing');
+app.set('x-powered-by',false);
 
 app.get ('/users', function (req,res){
 
   var values = dataQuery(
      function (dataQuery){
         res.header ({
-            'Link': ['http://localhost:300/users?p'+dataQuery.data.info.page,'']
+            'Content-type':'application/json', 
+            'Link': '<http://localhost:300/users?p='+dataQuery.data.info.page+'>; rel="first"'+
+            ',<http://localhost:300/users?p='+dataQuery.data.info.page+'>; rel="last"',
+            'Cache-Control':'private',
+            'Last-Modifield':moment().add(1, 'days')
 
         });
 
-        console.log(dataQuery.data.info);
 
+        res.json(dataQuery.data.results);
+        
      }
 
   );
-
-
-
 
 });
 
@@ -30,3 +36,5 @@ app.get ('/users', function (req,res){
 app.listen('300');
 
 
+//https://developer.github.com/v3/#paginationy
+//https://github.com/thlorenz/parse-link-header
